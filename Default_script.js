996d40a17436e58b2755bdbd3f127e3da82c2c9f
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2023-01-04 17:19:05 +0300

    Update Default_script.js

--- Default_script.js
+++ Default_script.js
@@ -2,32 +2,50 @@ document.onreadystatechange = function(){
     if(document.readyState !== "complete"){
         document.querySelector("body").style.visibility = "hidden";
         document.querySelector("#loading").style.visibility = "visible";
+        document.querySelector("body").style.height = "100%";
     }
     else{
         document.querySelector("#loading").style.display = "none";
         document.querySelector("body").style.visibility = "visible";
+        document.querySelector("body").style.height = "auto";
     }
 };
 /*function copyToClipboard(){
-    var copyText = document.getElementById("content-copy");
+var copyText = document.getElementById("content-copy");
 
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
+// Select the text field
+copyText.select();
+copyText.setSelectionRange(0, 99999); // For mobile devices
 
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
+// Copy the text inside the text field
+navigator.clipboard.writeText(copyText.value);
 
-    // Alert the copied text
-    alert("Copied: " + copyText.value);
-    }*/
-    window.onscroll = function()
+// Alert the copied text
+alert("Copied: " + copyText.value);
+}*/
+window.onscroll = function()
+{
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
     {
-        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+        document.getElementById('back-top').style.visibility="visible";
+    }else
+    {
+        document.getElementById('back-top').style.visibility="hidden";
+    }
+    var element_rel = document.getElementById("card-container");
+    var screenHeight =  screen.height;
+    var top_pos = element_rel.offsetTop - screenHeight / 1.2;
+    var element_abs = document.getElementById("card-content");
+    var i;
+    for(i = 100; i <= 107; i++){
+        if(document.body.scrollTop > top_pos || document.documentElement.scrollTop > top_pos)
         {
-            document.getElementById('back-top').style.visibility="visible"
-        }else
-        {
-            document.getElementById('back-top').style.visibility="hidden";
+            document.getElementById(i).style.top="0%";
         }
-    };
+        else{
+            document.getElementById(i).style.top="100%";
+        }
+    }
+    
+    
+};
