89a0fff4c762c0c070f41f2dfaaf10cb1b627eca
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2023-01-06 20:37:10 +0300

    Update Default_script.js

--- Default_script.js
+++ Default_script.js
@@ -38,18 +38,19 @@ window.onscroll = function()
     {
         document.getElementById('back-top').style.visibility="hidden";
     }
-    function top(){
+    
+    function slideInAnimation(){
+        var element_rel = document.getElementById("card-container");
+        var screenHeight =  document.body.offsetHeight;
+        var top_pos, card_desc;
+        top_pos = element_rel.offsetTop - screenHeight / 4.5;
         if(document.body.offsetWidth > 650){
-            var element_rel = document.getElementById("card-container");
-            var screenHeight =  document.body.offsetHeight;
-            var top_pos;
-            top_pos = element_rel.offsetTop - screenHeight / 4.5;
             for(let id = 200;id <=207;id++){
-                var card_desc = document.getElementById(id);
+                card_desc = document.getElementById(id);
                 card_desc.classList.add("card-desc-desktop");
             }
             var i = 100;               
-            function myLoop() {         
+            function topLoop() {         
             setTimeout(function() { 
                 if(document.body.scrollTop > top_pos || document.documentElement.scrollTop > top_pos)
                 {
@@ -60,17 +61,47 @@ window.onscroll = function()
                 }                      
                 i++;                    
                 if (i <= 107) {           
-                    myLoop();             
+                    topLoop();             
                 }
                 }, 125)
             }
-            myLoop();
-        }else{
-            card_desc.classList.remove("card-desc-desktop");
+            topLoop();
+        }
+        else{
+            for(let id = 200;id <=207;id++){
+                card_desc = document.getElementById(id);
+                card_desc.classList.remove("card-desc-desktop");
+            }
+            
+            var i = 100;                
+            function leftLoop() {        
+            setTimeout(function() {
+                if(document.body.scrollTop > top_pos || document.documentElement.scrollTop > top_pos)
+                {
+                    document.getElementById(i).style.left =  "0%";
+                }
+                else{
+                    for(let k = 100; k <= 107; k++){
+                        if(k % 2 == 0){
+                            document.getElementById(k).style.left = "-100%";   
+                        }
+                        else{
+                            document.getElementById(k).style.left = "100%";
+                        }
+                    }
+                }                   
+                i++;                    
+                if (i <= 107) {           
+                    leftLoop();             
+                }
+                }, 125)
+            }
+            leftLoop();
         }
     }
-    top();
-    window.addEventListener("resize", top);
+    slideInAnimation();
+    window.addEventListener("resize", slideInAnimation);
+
     var navbar = document.getElementById("navbar");
     var sticky = navbar.offsetTop;
     if (window.pageYOffset > sticky) {
